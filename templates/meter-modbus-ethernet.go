8ba41dc4ceb586cbed93257729888e0a9a2b94a1
premultiply <ulrich.peters@posteo.de>
2021-01-17 22:31:09 +0100

    Update meter-modbus-ethernet.go

--- templates/meter-modbus-ethernet.go
+++ templates/meter-modbus-ethernet.go
@@ -8,10 +8,10 @@ func init() {
 	template := registry.Template{
 		Class:  "meter",
 		Type:   "modbus",
-		Name:   "Modbus (Ethernet)",
+		Name:   "Eastron SDM Modbus Meter ('RTU-over-TCP')",
 		Sample: `model: sdm
 uri: 192.0.2.2:502
-rtu: true # rs485 device connected using ethernet adapter
+rtu: true # rs485 device connected using simple serial/tcp gateway
 id: 2
 power: Power # default values, optionally override
 energy: Sum # default values, optionally override`,
